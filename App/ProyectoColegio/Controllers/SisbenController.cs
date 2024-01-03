4a8370170295929cc7c2c601f03a00b48600f1c6
Daniel-RoGu <danrogu98@gmail.com>
2024-01-03 17:31:22 -0500

    commit_002

--- App/ProyectoColegio/Controllers/SisbenController.cs
+++ App/ProyectoColegio/Controllers/SisbenController.cs
@@ -2,10 +2,7 @@
 using ProyectoColegio.Data;
 using ProyectoColegio.Models;
 using System;
-using System.IO;
-using IronPython;
 using Microsoft.Scripting.Hosting;
-using System.Reflection;
 
 namespace ProyectoColegio.Controllers
 {
@@ -18,32 +15,64 @@ namespace ProyectoColegio.Controllers
             _contexto = contexto;
         }
 
-        
+        /*
+        [STAThread]
         public void RegistroSisben()
         {
             List<string> datos;
-            string pathPy = "~/Archivos_Python/InsercionSisben.py";
+            //string pathPy = "~/Archivos_Python/InsercionSisben.py";
 
-            // Añadir la referencia a la biblioteca IronPython
-            var assembly = Assembly.Load("IronPython.dll");
+            ScriptEngine engine = IronPython.Hosting.Python.CreateEngine();
+            ScriptRuntime runtime = engine.Runtime;
+            ScriptScope scope = runtime.CreateScope();
+            ScriptSource script = engine.CreateScriptSourceFromFile("~/Archivos_Python/InsercionSisben.py");
+            var compiled = script.Compile();
+            compiled.Execute(scope);
+            runtime.Shutdown();
 
-            // Importar la biblioteca IronPython
-            using (var scope = new ScriptScope(assembly))
-            {
-                // Crear un objeto ScriptEngine
-                var engine = new ScriptEngine(scope);
 
-                // Cargar el script .py
-                var script = File.ReadAllText(pathPy);
-                engine.Execute(script);
-
-                // Llama la función 
-                datos = engine.Invoke("resultado");
-            }
-
+            //ScriptRuntime py = Python.CreateRuntime();
+            //dynamic pyProgram = py.UseFile(pathPy);
+            //pyProgram.resultado();
 
             //return View();
+        }*/
+
+        [STAThread]
+        public void RegistroSisben()
+        {
+            List<string> datos;
+
+            // Crear el motor y el alcance
+            ScriptEngine engine = IronPython.Hosting.Python.CreateEngine();
+            ScriptRuntime runtime = engine.Runtime;
+            ScriptScope scope = runtime.CreateScope();
+
+            // Ejecutar el script y obtener el resultado
+            ScriptSource script = engine.CreateScriptSourceFromFile("~/Archivos_Python/InsercionSisben.py");
+            var compiled = script.Compile();
+            object result = compiled.Execute(scope);
+
+            // Utilizar un try-catch para manejar posibles excepciones
+            try
+            {
+                datos = script.Execute(scope);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al ejecutar el script: {ex.Message}");
+            }
+
+            /*
+            // Verificar si hay un resultado y mostrarlo
+            if (datos != null)
+            {
+                Console.WriteLine($"Resultado del script: {datos}");
+            }*/
+
+            // Cerrar el motor
+            runtime.Shutdown();
         }
-       
+
     }
 }
