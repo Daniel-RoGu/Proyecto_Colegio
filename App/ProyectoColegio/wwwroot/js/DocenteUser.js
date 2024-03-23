28bd48708bca7a610c92df55bfd886676637716c
diego1728 <44307913+diego1728@users.noreply.github.com>
2024-03-23 13:50:54 -0500

    commit-06 diego

--- App/ProyectoColegio/wwwroot/js/DocenteUser.js
+++ App/ProyectoColegio/wwwroot/js/DocenteUser.js
@@ -7,7 +7,6 @@ window.onload = function ()
 
 function listarGrados(valor) {
     fetchGet('Funcionario/ListarGradosDocente', function (data) {
-        console.log(data)
         if (data.length > 0) {
             const newArray = data.map((item) => ({
                 value: item[1],
@@ -80,4 +79,54 @@ function listarAsignaturasDocente(valor, grado) {
 
     })
 
+}
+
+
+function guardarEstudiante() {
+    let estudiante = document.getElementById('documenteEstudiante').value
+    let notas = document.getElementById('notasEstudiante').value;
+    let fallas = document.getElementById('fallaestudiante').value;
+
+    let frm = new FormData();
+    frm.append('', estudiante);
+    frm.append('', notas);
+    frm.append('', fallas);
+    fetchPostText('', frm, function (res) {
+
+        document.getElementById('cerraModalNotas').click();  
+
+    })
+
+
+
+
+
+
+}
+
+
+function limpiarNotasModal() {
+
+    borrarContenidoPorClase('notasEstudiante')
+
+}
+
+
+function borrarContenidoPorClase(clase) {
+
+    var inputs = document.querySelectorAll('.' + clase);
+
+    inputs.forEach(function (input) {
+
+        if (input.type === 'text' || input.type === 'textarea') {
+
+            input.value = '';
+        } else if (input.type === 'checkbox') {
+
+            input.checked = false;
+        } else {
+
+            input.value = '';
+        }
+    });
 }
